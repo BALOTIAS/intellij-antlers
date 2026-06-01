d4a46ea1b3ec694e906026e6a8d60bc2d500cadb
GitHub Action <action@github.com>
2026-06-01 07:29:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/balotias/intellijantlers/services/MyProjectService.kt
+++ src/main/kotlin/com/github/balotias/intellijantlers/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.balotias.intellijantlers.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.balotias.intellijantlers.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
